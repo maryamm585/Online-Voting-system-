a371c00eb6968b7a94b4a9e08a99c6b4679f796e
maryamhossam <112872236+maryamm585@users.noreply.github.com>
2025-08-12 19:32:22 +0300

    dtos done

--- voting/src/main/java/com/votingSystem/voting/dto/VoteDTO.java
+++ voting/src/main/java/com/votingSystem/voting/dto/VoteDTO.java
@@ -1,4 +1,20 @@
 package com.votingSystem.voting.dto;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class VoteDTO {
+      private Long id;
+
+    private LocalDateTime timeStamp; // can be set by backend when saving
+
+    @NotNull(message = "Voter ID is required")
+    private Long voterId;
+
+    @NotNull(message = "Candidate ID is required")
+    private Long candidateId;
 }
